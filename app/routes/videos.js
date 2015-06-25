66a2e6db4db96143bc481c0539dd926182d46b0e
adam mcfadden & Jessica Engel <student@epicodus.com>
2015-06-25 11:41:02 -0700

    allows users to click video icons and go to youtube page, will scroll through video thumbnails as the list grows.

--- app/routes/videos.js
+++ app/routes/videos.js
@@ -10,21 +10,7 @@ export default Ember.Route.extend({
     thumbnail: "https://i.ytimg.com/vi/az5qOjhsang/default.jpg",
     externalUrl: "https://www.youtube.com/watch?v=az5qOjhsang"
     },
-    watchedVideos: [{
-    id: "PUhuPn8_d0Q",
-    fullUrl: "http://www.youtube.com/embed/PUhuPn8_d0Q?autoplay=1&showinfo=0&controls=0&rel=0",
-    thumbnail: "https://i.ytimg.com/vi/PUhuPn8_d0Q/default.jpg",
-    externalUrl: "https://www.youtube.com/watch?v=PUhuPn8_d0Q"
-
-
-  }, {
-    id: "llNAsoF64Ng",
-    fullUrl: "http://www.youtube.com/embed/llNAsoF64Ng?autoplay=1&showinfo=0&controls=0&rel=0",
-    thumbnail: "https://i.ytimg.com/vi/llNAsoF64Ng/default.jpg",
-    externalUrl: "https://www.youtube.com/watch?v=llNAsoF64Ng"
-
-
-    }]
+    watchedVideos: []
     }
   }
 });
