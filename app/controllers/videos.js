5a35513d3d85f81f3dbbf3c85ac9044b6c4483ea
adam <student@epicodus.com>
2015-06-26 09:20:05 -0700

    fully featured, new seed each video

--- app/controllers/videos.js
+++ app/controllers/videos.js
@@ -63,75 +63,6 @@ function randomVideo(data) {
   });
 }
 
-var API_KEY = "AIzaSyDSW307ilh8iZL2gFIqR6R_nOoGpBoSc_Y";
-
-function videoURL(videoId) {
-  var videoUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics";
-  videoUrl += "&key=" + API_KEY;
-  return videoUrl + "&id=" + videoId;
-}
-
-function relatedVideosURL(videoId) {
-  var relatedVidsUrl = "https://www.googleapis.com/youtube/v3/search?maxResults=50&type=video&part=snippet"
-  relatedVidsUrl += "&key=" + API_KEY;
-  return relatedVidsUrl + "&relatedToVideoId=" + videoId;
-}
-
-function findNextVideo(videoId, steps, visitedVideoIds, controller) {
-  // debugger;
-  visitedVideoIds.push(videoId);
-  if (steps === 0) {
-
-    //need to add thumbnail to newVideo
-    var newVideo = {
-      id: videoId,
-      fullUrl: "http://www.youtube.com/embed/" + videoId + "?autoplay=1&showinfo=0&controls=0&rel=0",
-      externalUrl: "https://www.youtube.com/watch?v=" + videoId,
-      thumbnail   : "https://i.ytimg.com/vi/" + videoId + "/default.jpg"
-    }
-    controller.get('model.watchedVideos').pushObject(newVideo);
-    return;
-  }
-
-  getRelatedVideoIds(videoId)
-  .then(function(relatedVideoIds) {
-    return getRelatedVideoViews(relatedVideoIds)
-    .then(function(relatedVideos) {
-      var nextVideo = leastViewedRelatedVideo(relatedVideos, visitedVideoIds)
-      console.log("VideoId: " + nextVideo.id + ", views: " + nextVideo.views);
-      findNextVideo(nextVideo.id, steps - 1, visitedVideoIds, controller);
-    });
-  });
-}
-
-function getRelatedVideoIds(videoId) {
-  var url = relatedVideosURL(videoId);
-  return Ember.$.getJSON(url).then(function(responseJSON) {
-    return responseJSON.items.map(function(item) {return item.id.videoId;});
-  });
-}
-
-function getRelatedVideoViews(relatedVideoIds) {
-  return Promise.all(relatedVideoIds.map(function(relatedVideoId) {
-    return Ember.$.getJSON(videoURL(relatedVideoId)).then(function(responseJSON) {
-      return {
-        id: relatedVideoId,
-        views: parseInt(responseJSON.items[0].statistics.viewCount)
-      }
-    });
-  }));
-}
-
-function leastViewedRelatedVideo(videos, alreadyVisitedIds) {
-  var leastViewed = videos[0];
-  videos.forEach(function(video) {
-    if (video.views < leastViewed.views && alreadyVisitedIds.indexOf(video.id) === -1) {
-      leastViewed = video;
-    }
-  });
-  return leastViewed;
-}
-
 
 export default Ember.Controller.extend({
   watchedVideosCount: 0,
@@ -151,20 +82,10 @@ export default Ember.Controller.extend({
 
   actions: {
     nextVideo: function() {
-      this.incrementProperty('watchedVideosCount');
-      if (this.get('watchedVideosCount') % 3 === 0) {
-        var controller = this;
-        randomWord().then(function(video) {
-          controller.get('model.watchedVideos').pushObject(video);
-        });
-
-      } else {
-        console.log(this.get('videosWatched'));
-        var currentVideo = this.get('currentVideo');
-        console.log(currentVideo.id)
-        var watchedVideoIds = this.get('watchedVideoIds');
-        findNextVideo(currentVideo.id, 2, watchedVideoIds, this);
-      }
+      var controller = this;
+      randomWord().then(function(video) {
+        controller.get('model.watchedVideos').pushObject(video);
+      });
     }
   }
 });
